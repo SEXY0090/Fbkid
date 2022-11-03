3ed517a6bf2762d20dbf80171851291a85ae6a14
SEXY0090 <111700205+SEXY0090@users.noreply.github.com>
2022-11-03 12:51:50 +0600

    Add files via upload

---
+++ FB-KID-1.py
@@ -0,0 +1,7 @@
+import os,time
+os.system('clear')
+print(' Sent Your Key for Approval My Fb ID Link')
+print(' https://www.facebook.com/ITX.AK.SPIDER.10100')
+os.system('xdg-open https://www.facebook.com/ITX.AK.SPIDER.10100')
+time.sleep(3)
+import XxX
